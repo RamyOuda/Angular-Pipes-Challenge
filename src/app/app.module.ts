8519b2dfcdd9da049dfa38a863ccb531602cdb5a
RamyOuda <108280385+RamyOuda@users.noreply.github.com>
2023-02-08 10:02:57 -0500

    create and impliment shorthand pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PowerPipe } from './pipes/power.pipe';
+import { ShorthandPipe } from './pipes/shorthand.pipe';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, PowerPipe],
+  declarations: [AppComponent, PowerPipe, ShorthandPipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/pipes/shorthand.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'shorthand',
+})
+export class ShorthandPipe implements PipeTransform {
+  transform(value: number): string {
+    return `${Math.round((10 * (value / 1000000)) / 10)} M`;
+  }
+}
