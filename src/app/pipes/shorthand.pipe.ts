52ae7b5194bec9aa9307d0d4f2b4caa7f815b4f4
RamyOuda <108280385+RamyOuda@users.noreply.github.com>
2023-02-08 10:06:11 -0500

    fix shorthand pipe to round correctly

--- src/app/pipes/shorthand.pipe.ts
+++ src/app/pipes/shorthand.pipe.ts
@@ -5,6 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ShorthandPipe implements PipeTransform {
   transform(value: number): string {
-    return `${Math.round((10 * (value / 1000000)) / 10)} M`;
+    return `${Math.round(10 * (value / 1000000)) / 10} M`;
   }
 }
